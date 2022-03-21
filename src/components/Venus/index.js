7fbe8ab974c323164dc4928e6dc4ce69bbe02af0
Emilio Conde <condeemilio891@gmail.com>
2022-03-21 09:41:01 -0500

    card info done

--- src/components/Venus/index.js
+++ src/components/Venus/index.js
@@ -3,7 +3,7 @@ import GlassCard from "../GlassCard"
 import "./venus.css"
 import { faVenus } from "@fortawesome/free-solid-svg-icons"
 import { faXmarkSquare } from "@fortawesome/free-solid-svg-icons"
-import Hermes from "../../assets/Images/Hermes.webp"
+import Aphrodite from "../../assets/Images/Aphrodite.webp"
 
 class Venus extends React.Component{
     
@@ -24,16 +24,19 @@ return(
                     title="Venus"
             icon={faVenus}
             Xbutton={faXmarkSquare}
-            description=" Mercury appears as a morning or evening star. 
-             In Classical Greece it was called Apollo when it appeared as a morning star just before sunrise and Hermes, the Greek equivalent of the Roman god Mercury, when it appeared as an evening star just after sunset.
-            Hermes was the swift messenger of the gods, and the planet’s name is thus likely a reference to its rapid motions relative to other objects in the sky."
-            planetImage={Hermes}
+            description=" Venus is the brightest star in the night sky. Due to the planets luminous appearance, its name comes from 
+            Venus or Aphrodite, the Goddess of love and beauty. Although the planet appears to be beautiful from afar, it is a hostile place for life. 
+            The longest probe that has lasted on Venus's surface so far has been 2 hours. Unlike Mercury, which contains the most craters of any planet,
+            Venus crushes anything that goes inside its strong atmosphere. Also, there is a runaway green house effect. 
+            Carbon Dioxide and Sulfur retain so much heat that lead melts when it enters Venus's atmosphere.  
+              "
+            planetImage={Aphrodite}
             fact1="Longest Day in the Solar System 245 earth days 1 year on Venus is 225 days"
-            fact2="Temperature 864°F"
-            fact3= "No Moons"
-            fact4= "57,909,227 km (35,983,125 mi)from the Sun"
-            fact5="Orbital Period (1 Mercurial Year)is 88 days"
-            fact6= "Rotational Period (1 Earth Year) is 58.7 days"/>
+            fact2="Temperature 864°F Hottest Planet in the Solar System. "
+            fact3= "The atmosphere is  92x stronger than Earths."
+            fact4= "Earths closest Planet 22.5 million miles away."
+            fact5= "Billions of years ago Venus had a similar climate to Earth."
+            fact6= "Do to solar winds, temperature does not vary between night and day"/>
                     : null
                 }
 </>
