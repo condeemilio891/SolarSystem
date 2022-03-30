b424ec262409652cea02bb6720be13e241cb620d
Emilio Conde <condeemilio891@gmail.com>
2022-03-30 10:37:53 -0500

    API working and plugged in

--- src/components/Planets/Mars/index.js
+++ src/components/Planets/Mars/index.js
@@ -9,8 +9,31 @@ import Ares from "../../../assets/Images/Ares.webp"
 class Mars extends React.Component{
     
     state ={showing:false}
+    state = {weatherMax:''}
+    state ={weatherMin:''}
+    
+      componentDidMount(){
+        fetch("https://api.maas2.jiinxt.com/"  )
+        .then(res=>res.json())
+        
+        .then(
+          (result)=>
+          // console.log(result.max_temp),
+          {
+                  
+            this.setState({
+              weatherMax:result.max_temp,
+              weatherMin:result.min_temp
+              
+            })
+          }
+        )
+      }
+
   render(){
-    const { showing } = this.state;
+    const { showing,weatherMax,weatherMin } = this.state;
+ 
+
 return(
     <>
 <div class="mars">
@@ -24,12 +47,13 @@ return(
             icon={faMars}
             Xbutton={faXmarkSquare}
             description="This data is collected by the MAAS API. 
-            The high today is" 
+            The high and low today are" 
               
             planetImage={Ares}
-            fact1="Surface is Covered by Iron Oxide"
-            fact2="Summer Temperature is 63°F while Winter is -220°F"
-            fact3= "Has 2 moons: Phobos and Diemos"
+            fact1=" Below are the high and low temperatures '(Celsius) '
+            on mars according to the MAAS API"
+            fact2={weatherMax}
+            fact3= {weatherMin}
             fact4= "The biggest crater on Mars is Borealis Basin. It is 5300 miles from end to end and covers 40% of planet’s surface."
             fact5= "Polar Caps made of dry ice (frozen CO2)"
             fact6= "A day on Mars lasts 24 hours and 37 minutes."/>
