6446225c0968078f32ff1b005841146be591f986
Emilio Conde <condeemilio891@gmail.com>
2022-04-20 11:34:46 -0500

    nav bar working add, mars weather, jupiter and saturn systems.

--- src/components/SideBar/Navbar.js
+++ src/components/SideBar/Navbar.js
@@ -1,28 +1,37 @@
 import React, {useState} from 'react'
-import {FaBars} from "react-icons/fa"
+import * as FaIcons from "react-icons/fa"
 import { Link } from 'react-router-dom'
 import {AiOutlineClose} from "react-icons/ai"
+import { SideBarData } from './SidebarData'
+import "./Navbar.css"
 function Navbar () {
 
-  const [sidebar,setSidebar]= useState(false)
+  const [sidebar,setSidebar]= useState(false);
 
-  const showSidebar=()=> setSidebar(!sidebar)
+  const showSidebar=()=> setSidebar(!sidebar);
 
 
   return (
     <>
-        <div className='sidebar'>
+        <div className='navbar'>
             <Link to="#" className="menu-bars">
-                <FaBars onClick={showSidebar}/>
+                <FaIcons.FaBars onClick={showSidebar}/>
             </Link>
         </div>
-        <nav className={sidebar ? "nav-menu actve" : "nav-menu"}>
-          <ul className='nav-menu-items'>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
             <li className= "navbar-toggle">
-              <Link to="#" className='menu-bars'>
-                  <AiOutlineClose/>
-              </Link>
             </li>
+            {SideBarData.map((item,index)=>{
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })}
           </ul>
         </nav>
     </>
