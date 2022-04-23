0cf3ace1cadb827f60664a24463f5a10c1dba915
Emilio Conde <condeemilio891@gmail.com>
2022-04-23 00:49:52 -0500

    switch to mars weather and dwarf pplanets dropdown navbar next step.

--- src/components/SideBar/SidebarData.js
+++ src/components/SideBar/SidebarData.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import * as RiIcons from 'react-icons/ri'
 import * as GiIcons from 'react-icons/gi'
+import * as FaIcons from 'react-icons/fa'
 
 export const SideBarData=[
 
@@ -15,6 +16,18 @@ export const SideBarData=[
         title:'Dwarf Planets',
         path:'/dwarf',
         icon: <GiIcons.GiDwarfFace/>,
+        cName:'nav-text'
+    },
+    {
+        title:'Mars Weather',
+        path:'/marsweather',
+        icon:<FaIcons.FaMars/>,
+        cName:'nav-text'
+    },
+    {
+        title:'Saturn Moons',
+        path:'/saturnmoons',
+        icon: <GiIcons.GiRingedPlanet/>,
         cName:'nav-text'
     },
   
