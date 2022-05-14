445f2fcd4d7879957789bf9a87e78c35320d3567
Emilio Conde <condeemilio891@gmail.com>
2022-05-14 13:57:38 -0500

    Looking good

--- src/pages/SolarSystem.js
+++ src/pages/SolarSystem.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import useSound from 'use-sound'
+import { FaUserAstronaut } from 'react-icons/fa'
 import Container from "react-bootstrap/Container";
 import Video from "../assets/Videos/spaceVid.mp4";
 import Earth from "../components/Planets/Earth";
@@ -37,7 +38,7 @@ function SolarSystem() {
     setPeopleInSpace(res.data.people);
     setLoading(false)
   
-  console.log(res.data.people[0]);
+  console.log(res.data.people);
   
    })
    .catch(error => {
@@ -61,9 +62,13 @@ function SolarSystem() {
        <video className="videoTag" autoPlay loop muted>
             <source src={Video} type="video/mp4" />
           </video>
-          <h1 className="peopleInSpaceh1">People In space right now </h1>   
-          {peopleInSpace.map(({ name, craft }) => (
-        <p key={name} className="peopleInSpaceli"> {name} in the {craft} </p>
+          <h1 className="peopleInSpaceh1">People In  </h1>   
+          <h1 className="peopleInSpaceh1">Space Right Now </h1>
+          <h1 className="peopleInSpaceh1">by Astros API </h1>    
+          { peopleInSpace && peopleInSpace.map(({ name, craft }) => (
+            <div className="peopleInSpaceliCard">
+        <p key={name} className="peopleInSpaceli"><FaUserAstronaut/> {name} on the {craft} </p>
+        </div>
       ))}
               
           <Venus/>
